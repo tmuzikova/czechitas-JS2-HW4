3c2dfbae3beb09f10a83394daf0a2051afb50197
tmuzikova <tereza.muzikova@gmail.com>
2024-05-23 14:26:49 +0200

    styling added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,12 @@
-import { render } from '@czechitas/render';
-import '../global.css';
-import './index.css';
+import { render } from "@czechitas/render";
+import "../global.css";
+import "./index.css";
+import { BookingSummary } from "../components/BookingSummary";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-document.querySelector('#root').innerHTML = render(
+document.querySelector("#root").innerHTML = render(
   <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
-    </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v JavaScriptu s JSX. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-kodim-app">create-kodim-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
+    <BookingSummary />
   </div>
 );
